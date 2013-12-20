a91027bbfdd824cc0eac1476051e4c4feeaa2908
Gregory Burgess <epy00n@epy00n-VirtualBox.(none)>
2013-12-19 22:41:08 -1000

    Added an hourly update method.

--- uhmp/models.py
+++ uhmp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+# Represents an overall parking zone, which contains many areas
 class Parking(models.Model):
     name = models.CharField(max_length=200)
     STATUS_CHOICES = (("Open", "Open"), ("Permit Only", "Permit Only"), ("Full", "Full"))
@@ -8,6 +8,7 @@ class Parking(models.Model):
     def __unicode__(self):
         return str(self.name)
 
+# Represents an area within a parking zone
 class Area(models.Model):
     name= models.CharField(max_length=200)
     STATUS_CHOICES = (("Empty", "Empty"), ("Half-Full", "Half-Full"), ("Mostly-Full", "Mostly-Full"), ("Full", "Full"))
@@ -17,6 +18,7 @@ class Area(models.Model):
     def __unicode__(self):
         return str(self.name)
 
+# A user vote for the status of a given Parking zone at a given time
 class ParkingStatus(models.Model):
     zone = models.ForeignKey('Parking')
     status = models.CharField(max_length=30, choices=Parking.STATUS_CHOICES, default="Open")
@@ -24,6 +26,7 @@ class ParkingStatus(models.Model):
     def __unicode__(self):
         return self.zone.name + "   " + self.status + "   " + str(self.timestamp)[0:-13] 
 
+# A user vote for the status of a given Area at a given time
 class AreaStatus(models.Model):
     area = models.ForeignKey('Area')
     status = models.CharField(max_length=30, choices=Area.STATUS_CHOICES, default="Empty")
@@ -31,19 +34,23 @@ class AreaStatus(models.Model):
     def __unicode__(self):
         return self.area.name + "   " + self.status + "   " + str(self.timestamp)[0:-13] 
 
+# The status of a Parking zone at a given day of the week & hour
 class ParkingHist(models.Model):
     zone = models.ForeignKey('Parking')
     status = models.CharField(max_length=30, choices=Parking.STATUS_CHOICES, default="Open")
     dow = models.IntegerField()
     hour = models.IntegerField()
+    data= models.CharField(max_length=300)
     def __unicode__(self):
         return self.zone.name + "   " + str(self.dow) + " : " + str(self.hour) + "   " + self.status 
 
+# The status of an Area at a given day of the week & hour
 class AreaHist(models.Model):
     area = models.ForeignKey('Area')
     status = models.CharField(max_length=30, choices=Area.STATUS_CHOICES, default="Empty")
     dow = models.IntegerField()
     hour = models.IntegerField()
+    data= models.CharField(max_length=300)
     def __unicode__(self):
         return self.area.name + "   " + str(self.dow) + " : " + str(self.hour) + "   " + self.status 
 
--- uhmp/views.py
+++ uhmp/views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from django.utils import timezone
 from django.utils.timezone import utc
 from django.views.decorators.csrf import csrf_exempt
 from uhmp.settings import STATIC_URL
@@ -9,7 +10,9 @@ from uhmp.models import Parking, Area, ParkingStatus, AreaStatus, ParkingHist, A
 import datetime, operator
 
 
+#request the status page
 def status(request):
+	#calculate the status of each parking zone
         getCounts("status")
         return render_to_response(
               'status.html',
@@ -21,9 +24,12 @@ def status(request):
     )
 
 
-def lelist(request, currentZone=None):  
+#request the list page
+def lelist(request, currentZone=None):
+    #set default zone if no zone is specified
     if currentZone is not None:
 	currentZone = Parking.objects.get(name=currentZone)
+    # otherwise use the provided zone
     else:
 	currentZone = Parking.objects.first()
     areas = currentZone.area_set.order_by('floor', 'name')
@@ -39,10 +45,12 @@ def lelist(request, currentZone=None):
     )
 
 
+#request the graph page
 def graph(request):    
     parking = Parking.objects.all()
     area = Area.objects.all()
     places = list()
+    # compile a list of all Parking and Area objects
     for item in parking:
         places.append(item.name)
     for item in area:
@@ -58,6 +66,8 @@ def graph(request):
                }
     )
 
+
+# requeset a graph for a given day at a given time
 def getgraph(request, place, time):
     time = time.lower()
     if time == "sun":
@@ -76,16 +86,17 @@ def getgraph(request, place, time):
         time=6
 
     data = None
+    # check if the provided place is a Parking object
     try:
     	obj = Parking.objects.get(name=place)
         data = ParkingHist.objects.filter(zone=obj, dow=time)
 	oType = 'parking'
-
+    # check if the provided place is an Area object
     except Parking.DoesNotExist:
         obj = Area.objects.get(name=place)
 	data = AreaHist.objects.filter(area=obj, dow=time)
 	oType = 'area'
-
+    # build a js string with the appropirate data
     toRet = "<script>"
     toRet += "function drawVisualization() {"
     toRet += "var data = google.visualization.arrayToDataTable(["
@@ -102,17 +113,22 @@ def getgraph(request, place, time):
     toRet += "]);"
     toRet += "new google.visualization.LineChart(document.getElementById('graph')).draw(data, {"
     toRet += "title: 'Parking Status',"
+    toRet += "titleTextStyle: { color: 'white',fontName: 'arial',fontSize: 20,bold: true,italic: false },"
+    toRet += "titlePosition: 'in',"
     toRet += "curveType: 'none',"
     toRet += "width: '100%', height: 400,"
     toRet += "pointSize: 3,"
     toRet += "backgroundColor: 'black',"
-    toRet += "hAxis: {textStyle: { color: 'white',fontName: 'arial',fontSize: 15,bold: false,italic: false }},"
-    toRet += "vAxis: {textStyle: { color: 'white',fontName: 'arial',fontSize: 15,bold: false,italic: false }},"
+    toRet += "hAxis: {title: 'Hour', titleTextStyle: { color: 'white',fontName: 'arial',fontSize: 15,bold: false,italic: false }, "
+    toRet += "textStyle: { color: 'white',fontName: 'arial',fontSize: 15,bold: false,italic: false }},"
+    toRet += "vAxis: {title: 'Filled', textStyle: { color: 'white',fontName: 'arial',fontSize: 15,bold: false,italic: false }},"
     toRet += "legend: {textStyle: { color: 'white',fontName: 'arial',fontSize: 15,bold: false,italic: false }},"
     toRet += "vAxis: {maxValue: 90}}"
     toRet += ");}</script>"
     return HttpResponse(toRet)
 
+
+# helper method to translate text statuses into numbers
 def translate(status):
    toRet = ""
    if status == "Open" or status == "Empty":
@@ -126,14 +142,19 @@ def translate(status):
    return toRet
 
 
+# calculates the status of all objects of a given type
 def getCounts(objType):
-    now = datetime.datetime.utcnow().replace(tzinfo=utc)
+    # set the time interval to 15 mins
+    now = getTime()
+
     recent = now - datetime.timedelta(minutes=15)
     places = dict()
+    # request all Parking objects within the time interval
     if objType == "status":
         validEntries = ParkingStatus.objects.filter(timestamp__gte=recent)
         STATUS_CHOICES = Parking.STATUS_CHOICES
         keys = Parking.objects.all()
+    # request all Area objects within the time interval
     elif objType == "area":
         validEntries = AreaStatus.objects.filter(timestamp__gte=recent)
         STATUS_CHOICES = Area.STATUS_CHOICES
@@ -154,16 +175,63 @@ def getCounts(objType):
 	obj.save()
     return places
 
+# Stores the current hourly data for all Parking & Area objects into History objects
+def storeStatus():
+    # set the time interval to 1hr
+    now = getTime()
+    hour = now.hour
+    dow = now.isoweekday()
+    recent = now - datetime.timedelta(minutes=58)
+    print str(dow) + "  "+ str(hour)
+    # request all Parking objects within the time interval
+    validEntries = ParkingStatus.objects.filter(timestamp__gte=recent)
+    STATUS_CHOICES = Parking.STATUS_CHOICES
+    keys = Parking.objects.all()
 
+    for key in keys:
+	obj = keys.get(name=key.name)
+	hObj = ParkingHist.objects.get(zone=obj, dow=dow, hour=hour)
+        hist = eval(hObj.data)
+	# count up the votes for each option
+        for option, val in STATUS_CHOICES:
+            hist[option] = len(validEntries.filter(zone=key, status=option)) + hist[option]
+	# set the zone/area's current state to the winning option
+        hObj.data = repr(hist)
+
+	print "data" 
+	print hObj.data
+        hObj.status = sorted(hist.iteritems(), key=operator.itemgetter(1))[-1][0]
+	hObj.save()
+    # request all Area objects within the time interval
+    validEntries = AreaStatus.objects.filter(timestamp__gte=recent)
+    STATUS_CHOICES = Area.STATUS_CHOICES
+    keys = Area.objects.all()
+
+    for key in keys:
+ 	obj = keys.get(name=key.name)
+	hObj = AreaHist.objects.get(area=obj, dow=dow, hour=hour)
+        hist = eval(hObj.data)
+	# count up the votes for each option
+        for option, val in STATUS_CHOICES:
+            hist[option] = len(validEntries.filter(area=key, status=option)) + hist[option]
+	# set the zone/area's current state to the winning option
+        hObj.data = hist
+        hObj.status = sorted(hist.iteritems(), key=operator.itemgetter(1))[-1][0]
+	hObj.save()
+    return "success"
+
+# handles user votes/updates
 def update(request, objType, ID, status, currentZone=None):
     template = ""
-    now = datetime.datetime.utcnow().replace(tzinfo=utc)
+    now = getTime()
+    # create a status object for a corresponding Parking object
     if objType.lower() == "parking":
         zone = Parking.objects.get(id=ID)
         udo = ParkingStatus(zone=zone, status=status, timestamp=now)
         udo.save()
         getCounts("status")
         return redirect('/status')
+    # create a status object for a corresponding Area object
     elif objType.lower() == "area":
         area = Area.objects.get(id=ID)
         udo = AreaStatus(area=area, status=status, timestamp=now)
@@ -171,6 +239,40 @@ def update(request, objType, ID, status, currentZone=None):
 	getCounts("area")
         return redirect('/list/' + currentZone)
 
+# Returns the current Hawaii time
+def getTime():
+    timezone.activate('Pacific/Honolulu')
+    return timezone.localtime(timezone.now())
+
+
+# Creates a History object for Area and Parking objects
+# A helper for the initTimes() function
+def createHist(oType, ref, status, dow, hr):
+    P_STATUS_CHOICES = Parking.STATUS_CHOICES
+    A_STATUS_CHOICES = Area.STATUS_CHOICES
+    data = dict()
+
+    if oType == "parking":
+	hist = ParkingHist(zone=ref, status=status, dow=dow, hour=hr)
+        for option, val in P_STATUS_CHOICES:
+	    data[option] = 0
+        data[status] = 1
+        hist.data = data
+	hist.save()
+
+    elif oType == "area":
+	hist = AreaHist(area=ref, status=status, dow=dow, hour=hr)
+        for option, val in A_STATUS_CHOICES:
+	    data[option] = 0
+        data[status] = 1
+        hist.data = data
+	hist.save()
+	hist.save()
+
+
+
+
+# initalizes the database with base data for graphs
 def initTimes():
 	parkings = Parking.objects.all()
 	areas = Area.objects.all()
@@ -180,35 +282,24 @@ def initTimes():
 	for day in range(0,7):
 		for parking in parkings:
 			for hr in range(0,10):#0-9
-				hist = ParkingHist(zone=parking, status='Open', dow=day, hour=hr)
-				hist.save()
+				createHist("parking", parking, 'Open', day, hr)
 			for hr in range(10,12):#10-11
-					hist = ParkingHist(zone=parking, status='Permit Only', dow=day, hour=hr)
-					hist.save()
-			hist = ParkingHist(zone=parking, status='Full', dow=day, hour=12)
-			hist.save()
+				createHist("parking", parking, 'Permit Only', day, hr)
+			createHist("parking", parking, 'Full', day, 12)
 			for hr in range(13,15):#13-14
-					hist = ParkingHist(zone=parking, status='Permit Only', dow=day, hour=hr)
-					hist.save()
+					createHist("parking", parking, 'Permit Only', day, hr)
 			for hr in range(15,24):#15-23
-					hist = ParkingHist(zone=parking, status='Open', dow=day, hour=hr)
-					hist.save()
+					createHist("parking", parking, 'Open', day, hr)
+
 		for area in areas:
 			for hr in range(0,8):#0-7
-				hist = AreaHist(area=area, status='Empty', dow=day, hour=hr)
-				hist.save()
-			hist = AreaHist(area=area, status='Half-Full', dow=day, hour=8)
-			hist.save()
-			hist = AreaHist(area=area, status='Mostly-Full', dow=day, hour=9)
-			hist.save()
+				createHist("area", area, 'Empty', day, hr)
+			createHist("area", area, 'Half-Full', day, 8)
+			createHist("area", area, 'Mostly-Full', day, 9)
 			for hr in range(10,13):#10-12
-					hist = AreaHist(area=area, status='Full', dow=day, hour=hr)
-					hist.save()
-			hist = AreaHist(area=area, status='Mostly-Full', dow=day, hour=13)
-			hist.save()
+				createHist("area", area, 'Full', day, hr)
+			createHist("area", area, 'Mostly-Full', day, 13)
 			for hr in range(14,16):#14-15
-				hist = AreaHist(area=area, status='Half-Full', dow=day, hour=14)
-				hist.save()
+				createHist("area", area, 'Half-Full', day, hr)
 			for hr in range(16,24):#16-23
-					hist = AreaHist(area=area, status='Empty', dow=day, hour=hr)
-					hist.save()
+				createHist("area", area, 'Empty', day, hr)
